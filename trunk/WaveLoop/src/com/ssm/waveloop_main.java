7c1390cee3c7e483764ae23cbd602d6b676b7e57
lovelytien@gmail.com <lovelytien@gmail.com@4b6ec101-3738-1c0f-19b8-de19c5d30ac5>
2011-07-08 17:50:36 +0000

    email notification test 2

--- trunk/WaveLoop/src/com/ssm/waveloop_main.java
+++ trunk/WaveLoop/src/com/ssm/waveloop_main.java
@@ -22,6 +22,7 @@ public class waveloop_main extends TabActivity {
 	
 
     public void onCreate(Bundle savedInstanceState) {
+    	
     	super.onCreate(savedInstanceState);
     	
     	setContentView(R.layout.main);
