9d1e538d0170a2188d6553f6499c83db6f949517
kaieljin@gmail.com <kaieljin@gmail.com@4b6ec101-3738-1c0f-19b8-de19c5d30ac5>
2011-08-06 07:03:26 +0000

    리스트뷰에 커스텀어댑터를 통한 체크박스 연동.추가버튼 비활성화/활성화. 추가버튼 누르면 다이얼로그 생성 후 메인액티비티 리스트에 추가한 파일 추가됨.

--- trunk/WaveLoop/src/team/ssm/addActivity.java
+++ trunk/WaveLoop/src/team/ssm/addActivity.java
@@ -3,6 +3,7 @@ package team.ssm;
 import java.util.*;
 
 import team.ssm.DbAdapter.DatabaseHelper;
+import team.ssm.ImportProgressDialog.FinishLoading;
 import android.app.*;
 import android.content.*;
 import android.database.*;
@@ -10,7 +11,6 @@ import android.database.sqlite.*;
 import android.net.*;
 import android.os.*;
 import android.provider.MediaStore.Audio;
-import android.util.*;
 import android.view.*;
 import android.widget.*;
 
@@ -18,13 +18,11 @@ public class addActivity extends Activity {
 		Dialog dialog;
 	    AlertDialog.Builder builder;
 	    boolean[] mSelect;
-	    CharSequence[] mFiles;
+	    
 	 
 	    Cursor mCursor;
 	    ListView list2;
-	    //ArrayList<String> items;
-	    
-	    //ArrayList<sound> m_orders;
+	   	    
 	    ArrayList<sound> m_orders2;
 	    
 	    ArrayAdapter<sound> m_adapter2;
@@ -32,14 +30,16 @@ public class addActivity extends Activity {
 	    DbAdapter dba;
 	    DatabaseHelper dbh;
 	    SQLiteDatabase db;
-	    //int idx;
+	    
 	    ImportProgressDialog importDialog;
 	    int mPosition;
 	    Button btn;
-	    //CheckBox cb;
+	    CheckBox cb;
 	    
+	    private boolean isClick[] ; //체크박스의 체크유무를 저장하는 boolean 배열.
+	    ArrayList<String> mFiles;
 	    public static final String WAVEPATH = "/data/data/com.androidhuman.app/files/";   
-	
+	    private FinishLoading mFinishLoading;
 	
 	    public void onCreate(Bundle savedInstanceState) {
 		
@@ -54,14 +54,14 @@ public class addActivity extends Activity {
     	btn = (Button)findViewById(R.id.add);
 		btn.setEnabled(false);
 		
-        
+		
     	m_orders2 = new ArrayList<sound>();	//리스트뷰에 출력할 항목(sound 객체)들을 저장하는 ArraylList 생성.
         list2 = (ListView)findViewById(R.id.list2);
         m_adapter2 = new SoundAdapter2(this, R.layout.row2, m_orders2); // 어댑터를 생성.
         list2.setAdapter(m_adapter2);
         list2.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         list2.setOnItemClickListener(mItemClickListener2);
-        
+        list2.setItemsCanFocus(false);
         
        // CheckBox chk = (CheckBox)findViewById(R.id.chk);
         //chk.setOnCheckedChangeListener(mCheckChange);
@@ -88,7 +88,9 @@ public class addActivity extends Activity {
     	//mCursor = cursorExt;
     	int nCurCount = (mCursor == null)?0:mCursor.getCount();
     	if(nCurCount > 0){
-	    	for (int i = 0; i < nCurCount; i++) {
+    		isClick= new boolean[nCurCount];
+    		
+    		for (int i = 0; i < nCurCount; i++) {
 	    	
 	    		mCursor.moveToPosition(i);
 		    	String artist = mCursor.getString(mCursor.getColumnIndex(Audio.AudioColumns.ARTIST));
@@ -104,80 +106,84 @@ public class addActivity extends Activity {
     	
     	
 	}
-	    /*
-	    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-    		if (isChecked) {
-    			btn.setEnabled(true);
-    		}
-    		else {
-    			
-    		}
-	    }
-	     */
-	    /*
-	 // 어댑터뷰의 클릭리스너
-    AdapterView.OnItemClickListener mItemClickListener = new AdapterView.OnItemClickListener() {
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            /****************
-			리스트뷰에 나타나는 각각의 오디오 파일을 클릭했을 때 일어나는 동작을 정의하는 부분.
-            "파일을 클릭하면 재생 화면으로 넘어간다." 
-        	
-        	dba.open();
-        	//int idx = 0;
-        	Intent i = new Intent(addActivity.this, WaveLoopActivity.class); 
-        	//i.putExtra("오디오파일경로", position );
-        	//i.putExtra("오디오파일경로", path );
-        	
-        	
-        	startActivity(i);
-        }
-   };
-	*/
-	
-	
+	    
+	    
+	    
 	
 	
 	AdapterView.OnItemClickListener mItemClickListener2 = new AdapterView.OnItemClickListener() {
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            
-        	//Button btn = (Button)findViewById(R.id.add);
-			//btn.setEnabled(true);
-        	CheckBox chk = (CheckBox)findViewById(R.id.chk);
-        	chk.toggle();   
         	
+        	
+        	isClick[position] = ((ListView)parent).isItemChecked(position);
+        	int j=0;
+        	for(int i=0; i<isClick.length; i++){
+	        	if(isClick[i]){
+	        		j=j+1;
+	        		//btn.setEnabled(true);
+	        	}
+	        	
+        	}
+        	if(j>0){
+        		btn.setEnabled(true);
+        	}
+        	else{
+        		btn.setEnabled(false);
+        	}
+        	//CheckBox chk=(CheckBox)findViewById(R.id.chk);
+        	//isCheck[position] = chk.isChecked();
+        
         }
-   };
+	};
    
 	
    public void mOnClick2(View v) {
 	   switch(v.getId()){
 	   case R.id.add:
 		   
-		 /*  
-		   SparseBooleanArray sb = list2.getCheckedItemPositions();
+		 
+		// 선택된 음악파일 경로를 ArrayList에 담는다.
+			ArrayList<Integer> paths = new ArrayList<Integer>();
+			for(int i = 0; i < isClick.length; ++i )
+			{
+				if( isClick[i] )
+				{
+					mCursor.moveToPosition(i);	
+		            int id = mCursor.getInt(mCursor.getColumnIndex(Audio.AudioColumns._ID));
+					paths.add(id);
+				}
+			}
+			
+			
+			
+			// 로딩 다이얼로그 생성
+			importDialog = new ImportProgressDialog(addActivity.this);
+			importDialog.setAudioIDs(paths);
+			importDialog.setContentResolver(getContentResolver());
+			importDialog.setFinishLoading( new ImportProgressDialog.FinishLoading() { 
+				private ImportProgressDialog.EFinishResult mResult;
+				public void finish( ImportProgressDialog.EFinishResult result ){// dialog가 dismiss 될 때 호출되는 함수.
+					mResult = result;
+   				runOnUiThread( new Runnable(){
+   					public void run()
+   					{
+   						showLoadingResultMessage(mResult);
+   						//refreshListFromDB();
+   						Intent i = new Intent(addActivity.this, WaveLoopActivity.class);
+   			           startActivity(i);
+   					}
+   				});
+
+				}
+			});
+			importDialog.show();
+			importDialog.beginThread();
 		   
-			   for (int j =0;  j< list2.getCount();  j++) {
-                      
-                    	  sound s = m_orders2.get(j);
-                    	  String a = s.getArtist();
-                    	  Toast.makeText(this,"추가할 파일:", Toast.LENGTH_SHORT).show();
-                      
-               }
-		   
-		   */
-		   list2.clearChoices();
-           m_adapter2.notifyDataSetChanged();
-           Intent i = new Intent(addActivity.this, WaveLoopActivity.class);
-           startActivity(i);
+		   //list2.clearChoices();
+           //m_adapter2.notifyDataSetChanged();
+           //Intent i = new Intent(addActivity.this, WaveLoopActivity.class);
+           //startActivity(i);
   
-		   
-			     		//Intent i = new Intent(addActivity.this, WaveLoopActivity.class); 
-				   		//i.putExtra("체크된 항목 인덱스", idx);
-				   		//startActivity(i);
-				   		
-					  // Toast.makeText(v.getContext(), "체크됨", Toast.LENGTH_SHORT).show();
-					   
-				
 		   break;
 	   case R.id.cancel:
 		   	
@@ -186,7 +192,35 @@ public class addActivity extends Activity {
 	   
    }
    
+  
    
+	   private void showLoadingResultMessage( ImportProgressDialog.EFinishResult result )
+		{
+			String message;
+			switch(result)
+			{
+			case eFR_OK:
+				message = "성공적으로 처리되었습니다.";
+				break;
+			case eFR_FILEERROR:
+				message = "잘못된 파일입니다.";
+				break;
+			case eFR_FILENOTFOUNDERROR:
+				message = "파일을 찾을 수 없습니다.";
+				break;
+			case eFR_SUSPEND:
+				message = "작업이 중단되었습니다.";
+				break;
+			case eFR_EXCEPTION:
+				message = "알 수 없는 에러가 발생했습니다.";
+				break;
+			default:
+				message = "알 수 없는 에러가 발생했습니다.";
+				break;
+			}
+			Toast.makeText( getApplicationContext(), message, Toast.LENGTH_SHORT ).show();
+	  }
+
    
 	private class SoundAdapter2 extends ArrayAdapter<sound>{		
 
@@ -198,19 +232,21 @@ public class addActivity extends Activity {
         }
         
         // 각 항목의 뷰 생성
-        public View getView(int position, View convertView, ViewGroup parent) {
+        public View getView(final int position, View convertView, ViewGroup parent) {
                 View v = convertView;
                 if (v == null) {
                     LayoutInflater vi = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                     v = vi.inflate(R.layout.row2, null);
                 }
                 sound p = items.get(position);
+                //final int pos = position; 
                 if (p != null) {
                         
                 	
                 		TextView tar = (TextView) v.findViewById(R.id.top_artist2);
                         TextView tal = (TextView) v.findViewById(R.id.top_album2);
                         TextView bt = (TextView) v.findViewById(R.id.bottom_title2);
+                        CheckBox cb=(CheckBox)v.findViewById(R.id.chk);
                         if (tar != null){
                         	tar.setText(p.getArtist());                            
                         }
@@ -220,46 +256,17 @@ public class addActivity extends Activity {
                         if(bt != null){
                         		bt.setText(p.getTitle());
                         }
+                        if(cb != null){
+                        	//CheckBox cbx = (CheckBox) v.findViewById(R.id.chk);
+                            cb.setChecked(isClick[position]);
+                        	
+                            
+                        }
                         
                 }
-                
-               /* 
-               CheckBox cb = (CheckBox)v.findViewById(R.id.chk);
-               cb.setOnClickListener(new CheckBox.OnClickListener(){
-                	public void onClick(View v){
-                		btn.setEnabled(true);
-                	}
-                });
-                */
-                
-                
-        		
-        		CheckBox cb=(CheckBox)v.findViewById(R.id.chk);
-        		cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
-        			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-		        		if (isChecked) {
-		        			//cb.setText( "체크 상태" )
-		        			btn.setEnabled(true);
-		        			Toast.makeText(addActivity.this, "체크됨", Toast.LENGTH_SHORT).show();
-		        		}
-		        		else {
-		        			//cb.setText( "체크되지 않은 상태" );
-		        			Toast.makeText(addActivity.this, "해제됨", Toast.LENGTH_SHORT).show();
-		        		}
-        			}
-        		});
-				
-                /*
-				CheckBox cb = (CheckBox)v.findViewById(R.id.chk);
-				
-				if(cb != null){
-				cb.setChecked(v.isChecked()); // 체크 유무를 확인하여 체크
-				}
-                */
-                
+            
                 return v;
-              
-                
+    
         }
 
 		
@@ -296,9 +303,10 @@ public class addActivity extends Activity {
 	
     
     
-    public void onDestroy(Bundle savedInstanceState) {
-		super.onDestroy();
-		finish();
-	
-	}
+  
 }
+
+
+
+
+
