844b488cc08715d95a2637a031900ab149fd34a9
kaieljin <kaieljin@gmail.com>
2011-08-09 04:42:47 +0000

    HorizontalScrollView: HFling.java GestureListener: HFlingGestureListener.java

---
+++ WaveLoop/src/team/ssm/HFlingGestureListener.java
@@ -0,0 +1,38 @@
+package team.ssm;
+
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
+
+public class HFlingGestureListener extends SimpleOnGestureListener {
+	private HFling hf = null;
+	
+	public HFlingGestureListener(HFling hf) {
+		this.hf = hf;
+	}
+
+	@Override
+	public boolean onSingleTapConfirmed(MotionEvent e) {
+		hf.onClick();
+		return super.onSingleTapConfirmed(e);
+	}
+
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+			float velocityY) {
+		if(true == isLeft(e1,e2))
+			hf.onFlingLeft();
+		else
+			hf.onFlingRight();
+//		Log.i("GSL", "velocityX : " + velocityX);
+		return super.onFling(e1, e2, velocityX, velocityY);
+	}
+
+	// or you can check it by velocityX
+	private boolean isLeft(MotionEvent e1, MotionEvent e2) {
+		float result = e1.getX() - e2.getX(); 
+		if(0 < result)
+			return true;
+		return false;
+	}
+
+}
