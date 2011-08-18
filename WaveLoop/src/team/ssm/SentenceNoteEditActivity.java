8f8f7c24c2591e358c3716d8a61520ef28de97f4
lovelytien <lovelytien@gmail.com>
2011-08-18 07:48:06 +0000

    문장노트 star rate 출력및 수정 저장 완료. 문장노트 수정시 제목출력 완료.

--- WaveLoop/src/team/ssm/SentenceNoteEditActivity.java
+++ WaveLoop/src/team/ssm/SentenceNoteEditActivity.java
@@ -3,10 +3,14 @@ package team.ssm;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore.Audio;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,9 +19,10 @@ public class SentenceNoteEditActivity extends Activity {
 	TextView mTitleView;
 	TextView mTimeView;
 	EditText mNoteView;
+	RatingBar mRatingBar;
 	
 	DbAdapter dba;
-	int mSentenceRowID;
+	long mSentenceRowIndex;
 	
 	
 	
@@ -28,6 +33,7 @@ public class SentenceNoteEditActivity extends Activity {
     	mTitleView = (TextView)findViewById(R.id.sne_title);
     	mTimeView = (TextView)findViewById(R.id.sne_time);
     	mNoteView = (EditText)findViewById(R.id.sne_note);
+    	mRatingBar = (RatingBar)findViewById(R.id.sne_ratingBar);
     	
     	dba = new DbAdapter(this); //어댑터 객체 생성. 
     	
@@ -35,20 +41,45 @@ public class SentenceNoteEditActivity extends Activity {
     	Intent intent = getIntent();
         if (intent != null)
         {
-        	mSentenceRowID = intent.getIntExtra("sentence_row_id", 0);
+        	mSentenceRowIndex = intent.getIntExtra("sentence_row_id", 0);
         	
         	dba.open();
-        	Cursor cursor = dba.fetchBook2(mSentenceRowID);
+        	Cursor cursor = dba.fetchBook2(mSentenceRowIndex);
         	cursor.moveToPosition(0);
-        	String strMediaDBIndex = cursor.getString(cursor.getColumnIndex(DbAdapter.KEY_SENTENCE_MDB_ID));
+        	
+        	long mediaDBIndex = cursor.getLong(cursor.getColumnIndex(DbAdapter.KEY_SENTENCE_MDB_ID));
         	String strStartTime = cursor.getString(cursor.getColumnIndex(DbAdapter.KEY_START_TIME));
         	String strFinishTime = cursor.getString(cursor.getColumnIndex(DbAdapter.KEY_END_TIME));
         	String strNote = cursor.getString(cursor.getColumnIndex(DbAdapter.KEY_MEMO));
+        	int rate = cursor.getInt(cursor.getColumnIndex(DbAdapter.KEY_STAR_RATE));
+        	int color = cursor.getInt(cursor.getColumnIndex(DbAdapter.KEY_COLOR));
         	dba.close();
         	
+        	String title = "";
+			
+			Uri uriExternal = Audio.Media.EXTERNAL_CONTENT_URI;
+
+	    	String selection = "( (_ID LIKE ?) )";
+	    	String[] selectionArgs = { String.valueOf(mediaDBIndex) };
+	    	String sortOrder = Audio.Media.DEFAULT_SORT_ORDER;
+	    	 
+	    	
+	    	//Cursor cursorInt = mCr.query(uriInternal, null, selection, selectionArgs, sortOrder);
+	    	Cursor curMedia = getContentResolver().query(uriExternal, null, selection, selectionArgs, sortOrder);
+	    	if(curMedia.getCount() == 1)
+	    	{
+	    		curMedia.moveToPosition(0);
+		    	title = curMedia.getString(curMedia.getColumnIndex(Audio.AudioColumns.TITLE));
+	    	}
+
         	
+        	mTitleView.setText(title);
         	mTimeView.setText( "[" + strStartTime + " - " + strFinishTime + "]" );
         	mNoteView.setText(strNote);
+        	
+        	
+        	mRatingBar.setRating(rate/2.f);
+        	
         	
         	
 
@@ -64,6 +95,10 @@ public class SentenceNoteEditActivity extends Activity {
     	case R.id.sne_save:
 	    	
     		// 여기다가 저장좀 하고.
+    		dba.open();
+    		dba.updateBook2(mSentenceRowIndex, mNoteView.getText().toString(), String.valueOf((int)(mRatingBar.getRating()*2.f)) , "0");
+    		dba.close();
+    		
     		
     		Toast.makeText(getApplicationContext(), "저장 되었습니다.", Toast.LENGTH_SHORT ).show();
     		finish();
