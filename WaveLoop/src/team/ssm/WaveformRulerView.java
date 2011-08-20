7d50dede833fc46ebbec604d19743cd4f0b6e26d
lovelytien <lovelytien@gmail.com>
2011-08-20 17:23:12 +0000

    눈금 출력코드 수정.

--- WaveLoop/src/team/ssm/WaveformRulerView.java
+++ WaveLoop/src/team/ssm/WaveformRulerView.java
@@ -3,37 +3,60 @@ package team.ssm;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.Path;
 import android.view.View;
 
 public class WaveformRulerView extends View {
 	
-	private Paint mPaint;
+	
 	private int mWidth;
+	private static Path path;
+	private static Paint mPaint;
 	
 	public WaveformRulerView(Context context, int width) {
 		super(context);
 		
 		mPaint = new Paint();
-		mPaint.setColor(0xff74AC23);
+		mPaint.setColor(0xffffffff);
+		mPaint.setStyle(Style.STROKE);
 		
 		mWidth = width;
+		
+		if( path == null )
+		{
+			path = new Path();
+			path.moveTo(0, 0);
+			path.lineTo(mWidth*2, 0);
+			for(int i = 0; i < 10; ++i ){
+				path.moveTo(10*i, 0);
+				path.lineTo(10*i, (i%10==0)?15:10);
+			}
+		}
+		
+		
 		
 	}
 
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
 	
-		canvas.drawLine(0, 0, mWidth*2, 0, mPaint);
-		for(int i = 0; i < mWidth*2/20; ++i ){
-			canvas.drawLine(20*i, 0, 20*i, (i%5==0)?20:12, mPaint);
+		//canvas.drawLine(0, 0, mWidth*2, 0, mPaint);
+		//canvas.drawLine(0, 0, 0, 15, mPaint);
+		/*
+		for(int i = 0; i < 20; ++i ){
+			canvas.drawLine(5*i, 0, 5*i, (i%10==0)?15:10, mPaint);
 		}
+		*/
+		
+		canvas.drawPath(path, mPaint);
 
 	
 	}
 	
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
 	{
-		setMeasuredDimension(mWidth*2, 20);
+		setMeasuredDimension(mWidth*2, 15);
 	}
 
 
