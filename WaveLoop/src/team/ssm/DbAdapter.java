9aa758140f970f75a2053b4bcb487dbc80ced8b1
kaieljin <kaieljin@gmail.com>
2011-08-25 16:11:02 +0000

    DbAdapter에 문장노트db테이블에서 KEY_SENTENCE_MDB_ID를 인자로 받아 삭제하는 메소드 deleteBook2(sentence_mdb_id) 추가.

--- WaveLoop/src/team/ssm/DbAdapter.java
+++ WaveLoop/src/team/ssm/DbAdapter.java
@@ -141,6 +141,9 @@ public class DbAdapter {	//DB 어댑터. 데이터베이스에 접근하여 수�
 		return mDb.delete(DATABASE_TABLE2, KEY_ROWID2 + "=" + rowID, null) > 0;
 	}
 	
+	public boolean deleteBook2(String sentence_mdb_id){		//레코드 삭제(media_db_id)
+		return mDb.delete(DATABASE_TABLE, KEY_SENTENCE_MDB_ID + "=" + sentence_mdb_id, null) > 0;
+	}
 	
 	public void createTable() {	//테이블 생성.
 		mDb.execSQL(DATABASE_CREATE);
