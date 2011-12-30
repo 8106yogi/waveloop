dafbd9b6e0d4861242804b23ada7a917edfb7474
lovelytien@gmail.com <lovelytien@gmail.com@4b6ec101-3738-1c0f-19b8-de19c5d30ac5>
2011-12-30 17:21:11 +0000

    선택하지 않은 음악파일이 import 되는 버그 수정(벨소리 제거코드 관련 코드때문에) addActivity 검색필터 입력창 추가. addActivity 에서 화면회전시 메인으로 돌아가는 문제 수정.

--- trunk/WaveLoop/src/com/swssm/waveloop/addActivity.java
+++ trunk/WaveLoop/src/com/swssm/waveloop/addActivity.java
@@ -14,7 +14,10 @@ import android.database.sqlite.*;
 import android.net.*;
 import android.os.*;
 import android.provider.MediaStore.Audio;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.*;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.*;
 
 public class addActivity extends Activity {
@@ -26,9 +29,10 @@ public class addActivity extends Activity {
 	    Cursor mCursor;
 	    ListView list2;
 	   	    
-	    ArrayList<sound> m_orders2;
+	    ArrayList<Sound> mAllItems;
+	    private ArrayList<Sound> mFilteredItems;
 	    
-	    ArrayAdapter<sound> m_adapter2;
+	    ArrayAdapter<Sound> m_adapter2;
 	    ArrayList<String> MediaDBIndex;
 	    ArrayList<String> repetMDBIdx;
 	    String abc;
@@ -41,8 +45,10 @@ public class addActivity extends Activity {
 	    Button btn;
 	    CheckBox cb;
 	    
-	    private boolean isClick[] ; //체크박스의 체크유무를 저장하는 boolean 배열.(체크되면 true)
-	    private boolean isRepet[];	//파일의 중복유무를 저장하는 boolean 배열.(중복되면 true)
+	    private EditText mFilterText;
+	    
+	    private boolean isChecked[] ; //체크박스의 체크유무를 저장하는 boolean 배열.(체크되면 true)
+	    private boolean isImported[];	//파일의 중복유무를 저장하는 boolean 배열.(중복되면 true)
 	    private boolean isDismiss=false;
 	    ArrayList<String> mFiles;
 	    public static final String WAVEPATH = "/data/data/com.androidhuman.app/files/";   
@@ -62,10 +68,15 @@ public class addActivity extends Activity {
     	btn = (Button)findViewById(R.id.add);
 		btn.setEnabled(false);
 		
+		mFilterText = (EditText)findViewById(R.id.search_box);
+		mFilterText.addTextChangedListener(filterTextWatcher);
 		
-    	m_orders2 = new ArrayList<sound>();	//리스트뷰에 출력할 항목(sound 객체)들을 저장하는 ArraylList 생성.
+		
+    	mAllItems = new ArrayList<Sound>();	//리스트뷰에 출력할 항목(sound 객체)들을 저장하는 ArraylList 생성.
+    	mFilteredItems = mAllItems;
+    	
         list2 = (ListView)findViewById(R.id.list2);
-        m_adapter2 = new SoundAdapter2(this, R.layout.row2, m_orders2); // 어댑터를 생성.
+        m_adapter2 = new SoundAdapter2(this, R.layout.row2, mAllItems); // 어댑터를 생성.
         list2.setAdapter(m_adapter2);
         list2.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         list2.setOnItemClickListener(mItemClickListener2);
@@ -85,7 +96,7 @@ public class addActivity extends Activity {
     	//Uri uriInternal = Audio.Media.INTERNAL_CONTENT_URI;
     	 
 
-    	String selection = "( (_DATA LIKE ?) OR (_DATA LIKE ?) )";
+    	String selection = "( ( (_DATA LIKE ?) OR (_DATA LIKE ?) ) AND ( (IS_MUSIC = 1) OR (IS_PODCAST = 1) ) )";
     	String[] selectionArgs = {"%.mp3", "%.wav" };
     	String sortOrder = Audio.Media.DEFAULT_SORT_ORDER;
     	
@@ -96,8 +107,8 @@ public class addActivity extends Activity {
     	//mCursor = cursorExt;
     	int nCurCount = (mCursor == null)?0:mCursor.getCount();
     	if(nCurCount > 0){
-    		isClick= new boolean[nCurCount];
-    		isRepet= new boolean[nCurCount];
+    		isChecked = new boolean[nCurCount];
+    		isImported = new boolean[nCurCount];
     		
     		for (int i = 0; i < nCurCount; i++) {
 	    	
@@ -105,17 +116,10 @@ public class addActivity extends Activity {
 		    	String artist = mCursor.getString(mCursor.getColumnIndex(Audio.AudioColumns.ARTIST));
 				String album = mCursor.getString(mCursor.getColumnIndex(Audio.AudioColumns.ALBUM));
 				String title = mCursor.getString(mCursor.getColumnIndex(Audio.AudioColumns.TITLE));
-				int tTime = mCursor.getInt(mCursor.getColumnIndex(Audio.AudioColumns.DURATION))/1000;
+				int time = mCursor.getInt(mCursor.getColumnIndex(Audio.AudioColumns.DURATION))/1000;
 				
-				int isMusic = mCursor.getInt(mCursor.getColumnIndex(Audio.AudioColumns.IS_MUSIC));
-				int isPodcast = mCursor.getInt(mCursor.getColumnIndex(Audio.AudioColumns.IS_PODCAST));
-				
-				// 음악과 팟캐스트만 로드 가능.
-				if( isMusic != 0 || isPodcast != 0 )
-				{
-					sound s = new sound(artist, album, title, tTime);
-					m_orders2.add(s);
-				}
+				Sound s = new Sound(artist, album, title, time, mAllItems.size() );
+				mAllItems.add(s);
 	    		
 	    	}
 	    	startManagingCursor(mCursor);
@@ -123,6 +127,27 @@ public class addActivity extends Activity {
     	
     	compare();
 	}
+	    
+	    private TextWatcher filterTextWatcher = new TextWatcher() {
+
+	    	@Override
+	        public void beforeTextChanged(CharSequence s, int start, int count,
+	                int after) {
+	        }
+	        @Override
+	        public void onTextChanged(CharSequence s, int start, int before,
+	                int count) {
+	            //adapter.getFilter().filter(s);
+	        	m_adapter2.getFilter().filter(s);
+	        }
+
+	        @Override
+	        public void afterTextChanged(Editable s) {
+	            // TODO Auto-generated method stub
+
+	        }
+
+	    };
 	    
 	    public void onDestroy(){
 	    	super.onDestroy();
@@ -155,7 +180,7 @@ public class addActivity extends Activity {
 	            String id = mCursor.getString(mCursor.getColumnIndex(Audio.AudioColumns._ID)); //모든 오디오목록의 Audio.AudioColumns._ID를 저장하는 string 변수.
 				if(MediaDBIndex.contains(id)){
 					repetMDBIdx.add(id);
-					isRepet[j]=true;
+					isImported[j]=true;
 				}
 			}
 	  }
@@ -165,41 +190,49 @@ public class addActivity extends Activity {
 	AdapterView.OnItemClickListener mItemClickListener2 = new AdapterView.OnItemClickListener() {
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         	
+        	Sound s = mFilteredItems.get(position);
+        	isChecked[s.getIndex()] = ((ListView)parent).isItemChecked(position);
         	
-        	isClick[position] = ((ListView)parent).isItemChecked(position);
-        	int j=0;
-        	for(int i=0; i<isClick.length; i++){
-	        	if(isClick[i]){
-	        		j=j+1;
-	        		//btn.setEnabled(true);
-	        	}
-	        	
-        	}
-        	if(j>0){
-        		btn.setEnabled(true);
-        	}
-        	else{
-        		btn.setEnabled(false);
-        	}
-        	//CheckBox chk=(CheckBox)findViewById(R.id.chk);
-        	//isCheck[position] = chk.isChecked();
+        	updateAddButton();
+        }
         
+        private void updateAddButton()
+        {
+        	// 갯수를 세서 추가 버튼을 켜고끄는 작업을 하는듯.
+        	boolean hasChecked = false;
+        	for(int i = 0; i < isChecked.length; i++){
+	        	if( isChecked[i] )
+	        	{
+	        		hasChecked = true;
+	        		break;
+	        	}
+        	}
+
+    		btn.setEnabled( hasChecked );
+
         }
 	};
    
+	public void OnClick_FilterDelete(View v) {
+		mFilterText.setText("");
+		// 키보드 감추기
+		InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+		imm.hideSoftInputFromWindow(mFilterText.getWindowToken(), 0);
+
+	}
 	
-   public void mOnClick2(View v) {
+    public void mOnClick2(View v) {
 	   switch(v.getId()){
 	   case R.id.add:
 		   
 		 
 		// 선택된 음악파일 경로를 ArrayList에 담는다.
 			ArrayList<Integer> paths = new ArrayList<Integer>();
-			for(int i = 0; i < isClick.length; ++i )
+			for(int i = 0; i < isChecked.length; ++i )
 			{
-				if( isClick[i] )
+				if( isChecked[i] )
 				{
-					mCursor.moveToPosition(i);	
+					mCursor.moveToPosition(i);
 		            int id = mCursor.getInt(mCursor.getColumnIndex(Audio.AudioColumns._ID));
 					paths.add(id);
 				}
@@ -286,14 +319,16 @@ public class addActivity extends Activity {
 			Toast.makeText( getApplicationContext(), message, Toast.LENGTH_SHORT ).show();
 	  }
 
-   
-	private class SoundAdapter2 extends ArrayAdapter<sound>{		
+	
+	private class SoundAdapter2 extends ArrayAdapter<Sound> implements Filterable {		
 
-        private ArrayList<sound> items;
+		private SoundFilter filter;
+		private ArrayList<Sound> allItems;
+        
 
-        public SoundAdapter2(Context context, int textViewResourceId, ArrayList<sound> items) {
+        public SoundAdapter2(Context context, int textViewResourceId, ArrayList<Sound> items) {
                 super(context, textViewResourceId, items);
-                this.items = items;
+                this.allItems = items;
         }
         /*
         public boolean areAllItemsEnabled() {
@@ -301,7 +336,7 @@ public class addActivity extends Activity {
         }
         */
         public boolean isEnabled(int position) {
-        	if (isRepet[position])	//	position에 따른 조건을 주어 활성화/비활성화가 되게끔 코딩해야함. 
+        	if (isImported[position])	//	position에 따른 조건을 주어 활성화/비활성화가 되게끔 코딩해야함. 
         		return false;	// isRepet가 true이면, 즉 중복되었으면 비활성.
         	else
         		return true;	// 그렇지않으면, 즉 중복되지 않았으면 활성.
@@ -313,6 +348,10 @@ public class addActivity extends Activity {
         	int alpha = (isRepeat)?60:255;
         	v.setTextColor(v.getTextColors().withAlpha(alpha));
         }
+        public int getCount()
+        {
+        	return mFilteredItems.size();
+        }
         
         // 각 항목의 뷰 생성
         public View getView(final int position, View convertView, ViewGroup parent) {
@@ -321,7 +360,7 @@ public class addActivity extends Activity {
                     LayoutInflater vi = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                     v = vi.inflate(R.layout.row2, null);
                 }
-                sound p = items.get(position);
+                Sound p = mFilteredItems.get(position);
                 
                 if (p != null) {
 	        		TextView tar = (TextView) v.findViewById(R.id.top_artist2);
@@ -332,16 +371,16 @@ public class addActivity extends Activity {
 	                CheckBox cb=(CheckBox)v.findViewById(R.id.chk);
 	                if (tar != null){
 	                	tar.setText(p.getArtist());
-	                	setTextViewAlpha(tar, isRepet[position]);
+	                	setTextViewAlpha(tar, isImported[p.getIndex()]);
 	                	
 	                }
 	                if (tal != null){
 	                	tal.setText(p.getAlbum());
-	                	setTextViewAlpha(tal, isRepet[position]);
+	                	setTextViewAlpha(tal, isImported[p.getIndex()]);
 	                }
 	                if(bt != null){
 	                	bt.setText(p.getTitle());
-	                	setTextViewAlpha(bt, isRepet[position]);
+	                	setTextViewAlpha(bt, isImported[p.getIndex()]);
 	                    
 	                }
 	                if(rtt != null){
@@ -350,13 +389,13 @@ public class addActivity extends Activity {
         				int tSec = tot_time_sec % 60;
         			    String strTime = String.format("%02d:%02d" , tMin, tSec);
             			rtt.setText(strTime);
-            			setTextViewAlpha(rtt, isRepet[position]);
+            			setTextViewAlpha(rtt, isImported[p.getIndex()]);
                 }
 	                
 	                if(cb != null){
 	                	//CheckBox cbx = (CheckBox) v.findViewById(R.id.chk);
-	                    cb.setChecked(isClick[position]);
-	                    cb.setEnabled(!isRepet[position]);
+	                    cb.setChecked(isChecked[p.getIndex()]);
+	                    cb.setEnabled(!isImported[p.getIndex()]);
 	                	
 	                    
 	                }
@@ -364,27 +403,93 @@ public class addActivity extends Activity {
                 }
                 
                 return v;
-    
         }
+        
+        @Override
+        public Filter getFilter()
+        {
+            if (filter == null)
+            	filter  = new SoundFilter();
+            
+            return filter;
+        }
+        
+        
+
+        
+        private class SoundFilter extends Filter {
+
+            @SuppressWarnings("unchecked")
+            @Override
+            protected void publishResults(CharSequence prefix,
+                                          FilterResults results) {
+                // NOTE: this function is *always* called from the UI thread.
+                mFilteredItems = (ArrayList<Sound>)results.values;
+
+                notifyDataSetChanged();
+            }
+
+            @SuppressWarnings("unchecked")
+            protected FilterResults performFiltering(CharSequence prefix) {
+                // NOTE: this function is *always* called from a background thread, and
+                // not the UI thread. 
+            	
+            	String filterExp = ".*" + prefix.toString() + ".*";
+                FilterResults results = new FilterResults();
+
+                if ( prefix != null && prefix.toString().length() > 0 ) 
+                {
+                	ArrayList<Sound> filteredList = new ArrayList<Sound>();
+                    for (int i = 0; i < allItems.size(); i++)
+                    {
+                    	Sound s = allItems.get(i);
+                    	String title = s.getTitle().toLowerCase();
+                    	String album = s.getAlbum().toLowerCase();
+                    	String artist = s.getArtist().toLowerCase();
+                    	
+                    	if( title.matches(filterExp) ||
+                    		album.matches(filterExp) ||
+                    		artist.matches(filterExp) )
+                        {
+                            filteredList.add(s);
+                        }
+                    }
+                    results.values = filteredList;
+                    results.count = filteredList.size();
+                }
+                else
+                {
+                    synchronized (allItems)
+                    {
+                        results.values = allItems;
+                        results.count = allItems.size();
+                    }
+                }
+
+                return results;
+            }
+        }   
 
 		
     }
 	
     
     // 리스트 뷰에 출력할 항목
-    class sound {	
+    class Sound {	
         
         private String Artist;
         private String Album;
         private String Title;
-        private int tTime;
+        private int Time;
+        private int Index;
         
         
-        public sound(String _Artist, String _Album, String _Title, int _tTime) {
+        public Sound(String _Artist, String _Album, String _Title, int _Time, int _Index) {
         	this.Artist = _Artist;
         	this.Album = _Album;
         	this.Title = _Title;
-        	this.tTime = _tTime;
+        	this.Time = _Time;
+        	this.Index = _Index;
         }
         
         public String getArtist() {
@@ -398,7 +503,11 @@ public class addActivity extends Activity {
             return Title;
         }
         public int getTotalTime(){
-    	    return tTime;
+    	    return Time;
+    	}
+        
+        public int getIndex(){
+    	    return Index;
     	}
         
 
