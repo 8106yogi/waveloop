fa4af78ad377e2d073192b41668408235af5d0a0
lovelytien@gmail.com <lovelytien@gmail.com@4b6ec101-3738-1c0f-19b8-de19c5d30ac5>
2012-02-15 17:55:26 +0000

    현재 플레이중인 화면 계속 기능추가중. 글씨 그림자기능 제거.

--- trunk/WaveLoop/src/com/swssm/waveloop/AudioInfo.java
+++ trunk/WaveLoop/src/com/swssm/waveloop/AudioInfo.java
@@ -2,19 +2,13 @@ package com.swssm.waveloop;
 
 public class AudioInfo {
 	public long mediaId;
+	public long dataRowId;
 	public String title;
 	public String artist;
 	public String album;
 	public String path;
 	
-	public AudioInfo(int mediaId, String title, String artist, String album, String path)
-	{
-		this.mediaId = mediaId;
-		this.title = title;
-		this.artist = artist;
-		this.album = album;
-		this.path = path;
-	}
+	
 
 	public AudioInfo() {
 		
