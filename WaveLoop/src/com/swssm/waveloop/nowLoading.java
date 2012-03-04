3cb4c4535d57545083c3fa105c18c536e813997d
lovelytien <lovelytien@gmail.com>
2012-03-04 12:13:02 +0000

    로딩화면에 버전정보 출력.

--- WaveLoop/src/com/swssm/waveloop/nowLoading.java
+++ WaveLoop/src/com/swssm/waveloop/nowLoading.java
@@ -4,14 +4,29 @@ import com.swssm.waveloop.R;
 
 import android.app.*;
 import android.content.*;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.*;
 import android.view.*;
+import android.widget.TextView;
 
 public class nowLoading extends Activity {
+	private TextView mVersion;
 	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nowloading);
+        
+        Context context = getApplicationContext();
+        String version = null;
+        try {
+        	PackageInfo i = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+        	version = i.versionName;
+        } catch(NameNotFoundException e) { }
+        
+        mVersion = (TextView)findViewById(R.id.version);
+        mVersion.setText(version);
+        
         
         GlobalOptions.load(this);
         
@@ -26,6 +41,12 @@ public class nowLoading extends Activity {
 		
 		return true;
 	}
+	
+	/*
+	public boolean dispatchTouchEvent(MotionEvent event) {
+		return true;
+	}
+	*/
 	
 	
 	Handler mHandler = new Handler(){
